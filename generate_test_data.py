004ca95108fe06811ed5f411d0eef6685a427d58
Farkler <59152302+Farkler0@users.noreply.github.com>
2026-02-28 23:36:03 -0500

    Fix test data generation

--- generate_test_data.py
+++ generate_test_data.py
@@ -7,10 +7,10 @@ with open('user_game_data.csv', 'a+') as f:
             game = 'None'
         elif t - 1772254800 < 60 * 60 * 5.5:
             game = 'Stardew Valley'
-        elif t - 1772254800 < 60 * 60 * 9:
-            game = 'Balatro'
-        elif t - 1772254800 < 60 * 60 * 10:
+        elif t - 1772254800 < 25200:
             game = 'Minecraft'
         else:
             game = 'None'
         f.write(f'{t},{game}\n')
+    f.write(f'{1772280000 + 180},{'None'}\n')
+    
