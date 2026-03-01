31a34ddf6ecc56654bbc6f3150075ff51da003ee
Farkler <59152302+Farkler0@users.noreply.github.com>
2026-03-01 11:05:20 -0500

    Improve example data

--- generate_test_data.py
+++ generate_test_data.py
@@ -1,6 +1,6 @@
-
+open('user_game_data.csv', 'w').close()
 with open('user_game_data.csv', 'a+') as f:
-    for t in range(1772254800, 1772280000, 180):
+    for t in range(1772254800, 1772254800 + 50200 + 20000, 180):
         if t - 1772254800 < 60 * 60 * 1:
             game = 'Balatro'
         elif t - 1772254800 < 60 * 60 * 4:
@@ -9,8 +9,11 @@ with open('user_game_data.csv', 'a+') as f:
             game = 'Stardew Valley'
         elif t - 1772254800 < 25200:
             game = 'Minecraft'
+        elif t - 1772254800 < 25200 + 10000:
+            game = 'None'
+        elif t - 1772254800 < 25200 + 25000:
+            game = 'Valorant'
         else:
             game = 'None'
         f.write(f'{t},{game}\n')
-    f.write(f'{1772280000},{"None"}\n')
     
